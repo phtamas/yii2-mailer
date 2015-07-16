e6f7cfb9899032cd5ce9c4832db98f4933806c03
phtamas <phtamas@gmail.com>
2015-07-16 14:18:50 +0200

    Removed obsolete test

--- tests/unit/MailTest.php
+++ tests/unit/MailTest.php
@@ -5,42 +5,6 @@ use phtamas\yii2\mailer\Mail;
 
 class MailTest extends \PHPUnit_Framework_TestCase
 {
-    public function testSend()
-    {
-        /*$mailer = $this->getMockBuilder('Swift_Mailer')
-            ->disableOriginalConstructor()
-            ->getMock();
-        $mailer->expects($this->once())
-            ->method('send')
-            ->with($this->callback(function ($message) {
-                $this->assertInstanceOf('Swift_Message', $message);
-                $this->assertEquals('multipart/alternative', $message->getContentType());
-                $this->assertEquals('html body', $message->getBody());
-                $this->assertArrayHasKey(0, $message->getChildren());
-                $this->assertEquals('text/plain', $message->getChildren()[0]->getContentType());
-                $this->assertEquals('plain text body', $message->getChildren()[0]->getBody());
-                return true;
-            }));
-
-        $mail = new Mail('test-mail', $mailer);
-
-        $templateData = ['key' => 'value'];
-        $view = $this->getMockBuilder('yii\web\View')
-            ->disableOriginalConstructor()
-            ->getMock();
-        $view->method('render')
-            ->will($this->onConsecutiveCalls('plain text body', 'html body'));
-        $view->expects($this->exactly(2))
-            ->method('render')
-            ->withConsecutive(
-                [$this->equalTo('plain-text/test-mail'), $this->equalTo($templateData), $this->identicalTo($mail)],
-                [$this->equalTo('html/test-mail'), $this->equalTo($templateData), $this->identicalTo($mail)]
-            );
-        $mail->setView($view);
-
-        $mail->send($templateData);*/
-    }
-
     public function testBeginPlainTextBodyWithPlainTextMail()
     {
         $mail = new Mail('test-mail', $this->getMockBuilder('\Swift_Mailer')->disableOriginalConstructor()->getMock());
